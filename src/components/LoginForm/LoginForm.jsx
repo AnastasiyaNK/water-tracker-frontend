f8f5edb54b3716d73203aadb8d88075fa1d13e3e
zsribna <zsribna@gmail.com>
2024-01-26 22:05:24 +0200

    add login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  return <div className="App">
+    <h1>React form </h1>
+    <form>
+      <label> Sign In </label>
+
+      <label for="email">Enter your email</label>
+      <input type="email" id="email" name="email" required></input>
+       <label for="password">Enter your password</label>
+      <input type="password" id="password" name="password" required></input>
+      <button type="submit" value="Зареєструватися"> Sing Up</button>
+      
+    </form>
+  </div>;
 };
 
 export default LoginForm;
