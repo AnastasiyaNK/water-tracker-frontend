6bf2131210bf039c7f3442510c7fb00273993644
AnastasiyaNK <tsoka.anastasiya@gmail.com>
2024-01-27 09:52:28 +0100

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,4 @@ export { default as LoginForm } from "./LoginForm/LoginForm";
 export { default as RegisterForm } from "./RegisterForm/RegisterForm";
 
 export { SettingsModal } from "./SettingsModal/SettingsModal";
-
-
+export { default as Header } from "./Header/Header";
