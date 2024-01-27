d2cd9fe045421ea258815af42fd8490bd0def082
zsribna <zsribna@gmail.com>
2024-01-27 23:10:46 +0200

    update login form and register form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+//import React, { useState } from "react";
 
 const LoginForm = () => {
-  return <div className="App">
-    <h1>React form </h1>
-    <form>
-      <label> Sign In </label>
+ 
 
-      <label for="email">Enter your email</label>
-      <input type="email" id="email" name="email" required></input>
-       <label for="password">Enter your password</label>
-      <input type="password" id="password" name="password" required></input>
-      <button type="submit" value="Зареєструватися"> Sing Up</button>
-      
-    </form>
-  </div>;
+  return (
+    <div className="App">
+      <form >
+        <p>Enter your email</p>
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          
+        />
+        <p>Enter your password</p>
+        <input type="password" name="password" placeholder="Password"
+        />
+        <button type="submit">Sign in</button>
+      </form>
+    </div>
+  );
 };
 
+
+  
 export default LoginForm;
