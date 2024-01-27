5d04aef43e46e12c44b3b6f0fea621fe4e36dc3a
KanterIV <126972573+KanterIV@users.noreply.github.com>
2024-01-27 09:38:26 +0100

    added new styles to SettingsModal component
    1.With the help of styled-component added styling of the right part of the modal window and name and email inputs;
    2. Implemented a temporary solution to close the modal window.

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,4 @@
+export { default as Header } from "./Header/Header";
 export { default as LoginForm } from "./LoginForm/LoginForm";
 export { default as RegisterForm } from "./RegisterForm/RegisterForm";
-
-export { SettingsModal } from "./SettingsModal/SettingsModal";
-
-
+export { default as SettingsModal } from "./SettingsModal/SettingsModal";
