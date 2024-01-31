3f015457d77ed6680b9a981364e6cf7ffe5f7e72
Vika14 <manunya1406@gmail.com>
2024-01-31 17:58:58 +0100

    WelcomPage-3

--- src/components/WelcomePage/WelcomePage.styled.js
+++ src/components/WelcomePage/WelcomePage.styled.js
@@ -3,41 +3,7 @@ import { ReactComponent as CalendarIconSvg } from "../../assets/icons/calendar-d
 import { ReactComponent as SettingsIconSvg } from "../../assets/icons/wrench-screwdriver.svg";
 import { ReactComponent as StatisticsIconSvg } from "../../assets/icons/presentation-chart-bar.svg";
 
-import backgroundMobile from "../../assets/images/background-main-mob.png";
-import backgroundTablet from "../../assets/images/background-main-tab.png";
-import backgroundDesktop from "../../assets/images/background-main-desk.png";
-import bg_dt1x from "../../assets/images/bubble-desktop.png";
-
 export const WelcomeContainer = styled.div`
-  background-image: url(${backgroundMobile});
-  background-size: contain;
-  background-size: 100%;
-  background-position: center bottom;
-  background-repeat: no-repeat;
-
-  @media screen and (max-width: 767px) and (-webkit-min-device-pixel-ratio: 2),
-    (max-width: 767px) and (min-resolution: 192dpi) {
-    background-image: url(${backgroundMobile});
-  }
-
-  @media screen and (min-width: 768px) and (max-width: 1439px) {
-    background-image: url(${backgroundTablet});
-  }
-
-  @media screen and (min-width: 768px) and (-webkit-min-device-pixel-ratio: 2),
-    (min-width: 768px) and (min-resolution: 192dpi) {
-    background-image: url(${backgroundTablet});
-  }
-
-  @media screen and (min-width: 1440px) {
-    background-image: url(${backgroundDesktop}), url(${bg_dt1x});
-  }
-
-  @media screen and (min-width: 1440px) and (-webkit-min-device-pixel-ratio: 2),
-    (min-width: 1440px) and (min-resolution: 192dpi) {
-    background-image: url(${backgroundDesktop}), url(${bg_dt1x});
-  }
-
   width: 320px;
   min-height: calc(100vh - 80px);
   padding: 0 20px 40px 20px;
@@ -46,6 +12,7 @@ export const WelcomeContainer = styled.div`
   background-size: 100%;
   background-position: center bottom;
   background-repeat: no-repeat;
+  padding-top: 24px;
 
   @media screen and (max-width: 767px) and (-webkit-min-device-pixel-ratio: 2),
     (max-width: 767px) and (min-resolution: 192dpi) {
@@ -53,8 +20,7 @@ export const WelcomeContainer = styled.div`
 
   @media screen and (min-width: 768px) and (max-width: 1439px) {
     width: 768px;
-
-    padding: 0 32px 50px 32px;
+    padding-top: 40px;
     min-height: calc(100vh - 88px);
   }
 
@@ -67,7 +33,6 @@ export const WelcomeContainer = styled.div`
     justify-content: center;
     gap: 81px;
     width: 1440px;
-    padding: 0 18px 109px 18px;
     padding-top: 40px;
     min-height: calc(100vh - 80px);
   }
