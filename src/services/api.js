d037ac6984bcafd6773d87fe5385323ebf07408b
AnastasiyaNK <tsoka.anastasiya@gmail.com>
2024-02-03 20:52:53 +0100

    add logout

--- src/services/api.js
+++ src/services/api.js
@@ -1,36 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const authInstance = axios.create({
-  baseURL: 'https://water-tracker-backend-0wax.onrender.com/api',
+  baseURL: "https://water-tracker-backend-0wax.onrender.com/api",
 });
 
-export const setToken = token => {
+export const setToken = (token) => {
   authInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
-export const requestRegister = async formData => {
-  const { data } = await authInstance.post('user/register', formData);
+export const requestRegister = async (formData) => {
+  const { data } = await authInstance.post("user/register", formData);
   setToken(data.token);
   return data;
 };
 
-export const requestLogin = async formData => {
-  const { data } = await authInstance.post('user/login', formData);
+export const requestLogin = async (formData) => {
+  const { data } = await authInstance.post("user/login", formData);
   setToken(data.token);
   return data;
 };
 
-export const updateAvatar = async formData => {
-  const { data } = await authInstance.patch('user/avatar', formData, {
+export const updateAvatar = async (formData) => {
+  const { data } = await authInstance.patch("user/avatar", formData, {
     headers: {
-      'Content-Type': 'multipart/form-data',
+      "Content-Type": "multipart/form-data",
     },
   });
   return data;
 };
 
-export const updateUser = async formData => {
-  const { data } = await authInstance.patch('user/update', formData);
+export const updateUser = async (formData) => {
+  const { data } = await authInstance.patch("user/update", formData);
   return data;
 };
 
@@ -38,9 +38,9 @@ export const updateUser = async formData => {
 
 export const getWaterNotes = async () => {
   const currentDate = new Date();
-  const formattedDate = currentDate.toISOString().split('T')[0];
+  const formattedDate = currentDate.toISOString().split("T")[0];
 
-  const { data } = await authInstance.get('/water/today', {
+  const { data } = await authInstance.get("/water/today", {
     params: {
       date: formattedDate,
     },
@@ -49,12 +49,17 @@ export const getWaterNotes = async () => {
   return data;
 };
 
-export const getWaterStats = async month => {
+export const getWaterStats = async (month) => {
   const { data } = await authInstance.get(`/water/month?${month}`);
   return data;
 };
 
-export const deleteWaterNote = async id => {
+export const deleteWaterNote = async (id) => {
   const { data } = await authInstance.delete(`/water/delete/${id}`);
+  return data;
+};
+
+export const requestlogout = async () => {
+  const { data } = await authInstance.post("user/logout");
   return data;
 };
