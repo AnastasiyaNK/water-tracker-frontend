8bf9fc0a9e2a2c085879ba3f9c797480b6655760
AnastasiyaNK <tsoka.anastasiya@gmail.com>
2024-02-07 12:52:22 +0100

    add lazy-loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { PrivateRoute, RestrictedRoute, SharedLayout } from "components";
 import { ROUTE_PATH } from "constants/routes";
-import { HomePage, SignInPage, SignUpPage, WelcomePage } from "pages";
-import { useEffect } from "react";
+import { Suspense, lazy, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { Navigate, Route, Routes } from "react-router-dom";
@@ -9,6 +8,11 @@ import { usersCurrentThunk } from "./redux/userSlice";
 import { selectUserIsLoading } from "./redux/selectors";
 import { selectWaterIsLoading } from "./redux/water/waterSlice.selectors";
 import Loader from "components/Loader/Loader";
+
+const WelcomePage = lazy(() => import("./pages/WelcomePage/WelcomePage"));
+const HomePage = lazy(() => import("pages/HomePage/HomePage"));
+const SignInPage = lazy(() => import("pages/SignInPage/SignInPage"));
+const SignUpPage = lazy(() => import("pages/SignUpPage/SignUpPage"));
 
 function App() {
   const dispatch = useDispatch();
@@ -22,46 +26,48 @@ function App() {
   return (
     <div>
       <SharedLayout>
-        <Routes>
-          <Route
-            path={ROUTE_PATH.index}
-            element={<Navigate to={ROUTE_PATH.welcome} replace />}
-          />
-          <Route
-            path={ROUTE_PATH.welcome}
-            element={
-              <RestrictedRoute>
-                <WelcomePage />
-              </RestrictedRoute>
-            }
-          />
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route
+              path={ROUTE_PATH.index}
+              element={<Navigate to={ROUTE_PATH.welcome} replace />}
+            />
+            <Route
+              path={ROUTE_PATH.welcome}
+              element={
+                <RestrictedRoute>
+                  <WelcomePage />
+                </RestrictedRoute>
+              }
+            />
 
-          <Route
-            path={ROUTE_PATH.signup}
-            element={
-              <RestrictedRoute>
-                <SignUpPage />
-              </RestrictedRoute>
-            }
-          />
-          <Route
-            path={ROUTE_PATH.signin}
-            element={
-              <RestrictedRoute>
-                <SignInPage />
-              </RestrictedRoute>
-            }
-          />
+            <Route
+              path={ROUTE_PATH.signup}
+              element={
+                <RestrictedRoute>
+                  <SignUpPage />
+                </RestrictedRoute>
+              }
+            />
+            <Route
+              path={ROUTE_PATH.signin}
+              element={
+                <RestrictedRoute>
+                  <SignInPage />
+                </RestrictedRoute>
+              }
+            />
 
-          <Route
-            path={ROUTE_PATH.home}
-            element={
-              <PrivateRoute>
-                <HomePage />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
+            <Route
+              path={ROUTE_PATH.home}
+              element={
+                <PrivateRoute>
+                  <HomePage />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Suspense>
       </SharedLayout>
       {isLoadingUser || isLoadingWater ? <Loader /> : null}
     </div>
